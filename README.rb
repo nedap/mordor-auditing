3f0b7245917f47b847d8ebc8d445245dadcfd7a2
J.W. Koelewijn <jwkoelewijn@gmail.com>
2012-06-21 14:21:11 +0200

    Added small readme

---
+++ README.rb
@@ -0,0 +1,5 @@
+[![Build Status](https://secure.travis-ci.org/jwkoelewijn/auditing.png?branch=master)](http://travis-ci.org/jwkoelewijn/auditing)
+
+## Introduction
+
+Small library that builds on the Mordor gem to provide auditing classes
