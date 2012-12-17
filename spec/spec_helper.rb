95c2b3b904fad774f34bab09199a0476ef4efc3f
J.W. Koelewijn <jwkoelewijn@gmail.com>
2012-12-17 11:33:35 +0100

    Updated spec helper to first set mordor configuration, before including mordor resources

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,24 @@
 require 'rubygems'
 require 'mongo'
 require 'mordor'
-
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-require 'auditing'
 
 Mordor::Config.use do |config|
- config[:database] = 'test'
+  config[:username] = nil
+  config[:password] = nil
+  config[:hostname] = '127.0.0.1'
+  config[:port] = 27017
+  config[:database] = 'test'
 end
+
+require 'auditing'
+
 
 def clean_sheet
   @connection = Mongo::Connection.new(Mordor::Config[:hostname], Mordor::Config[:port])
   @db = @connection[Mordor::Config[:database]]
   [Auditing::Request, Auditing::Modification].each do |klass|
+    @db[klass.collection_name].remove
     @db[klass.collection_name].drop
   end
 end
